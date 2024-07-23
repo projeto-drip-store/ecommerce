10807c2742bcddb819e4750f9f3ffaec9405303f
JeffersonGuedes <jeffersonguedes0211@gmail.com>
2024-07-22 21:15:19 -0300

    :lipstick: Estilizando a tela que estava vazando da pagina

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,6 +14,35 @@ function Home() {
     <>
       <Header />
       <Carousel />
+      <section className="colecao-destaque-icons">
+        <div className="colecao-destaque-icons-escopo">
+          <div className="titulo-colecao-destaque">
+            <h1>Coleções em destaque</h1>
+          </div>
+          <div className="cards-colecao-destaque">
+            <div className="blusa-destaque">
+              <img src="" alt="blusa"/>
+              <h4>Camisetas</h4>
+            </div>
+            <div className="calcas-destaque">
+              <img src="" alt="blusa"/>
+              <h4>Camisetas</h4>
+            </div>
+            <div className="bone-destaque">
+              <img src="" alt="blusa"/>
+              <h4>Camisetas</h4>
+            </div>
+            <div className="headphone-destaque">
+              <img src="" alt="blusa"/>
+              <h4>Camisetas</h4>
+            </div>
+            <div className="tenis-destaque">
+              <img src="" alt="blusa"/>
+              <h4>Camisetas</h4>
+            </div>
+          </div>
+        </div>
+      </section>
       <section className="container-produtos-em-alta">
         <div className="produtos-em-alta">
           <div className="topico-section">
