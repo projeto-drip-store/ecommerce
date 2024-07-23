5eaccd4938c7be9c29a3af3531070b2f7cc09ed2
JeffersonGuedes <jeffersonguedes0211@gmail.com>
2024-07-23 20:38:52 -0300

    :snake: feat: Criando component de coleções em destaque - foi alterado a rota do erro404 - adicionei svgs do icons da coleção em destaque - foi criado um novo components IconDestaque para HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Carousel from "../Components/Carousel/Carousel"
 import Footer from "../Components/Footer/Footer"
 import Cards from "../Components/Cards/Cards"
 import Cards2 from "../Components/Cards/Cards2"
+import IconDestaque from "../Components/IconDestaque/IconDestaque"
 import sapatoAzul from "../assets/img/sapato_card.png"
 import flechaRosa from "../assets/img/flecha_icon.svg"
 import "./HomePage.css"
@@ -15,35 +16,7 @@ function Home() {
     <>
       <Header />
       <Carousel />
-      <section className="colecao-destaque-icons">
-        <div className="colecao-destaque-icons-escopo">
-          <div className="titulo-colecao-destaque">
-            <h1>Coleções em destaque</h1>
-          </div>
-          <div className="cards-colecao-destaque">
-            <div className="blusa-destaque">
-              <img src="" alt="blusa"/>
-              <h4>Camisetas</h4>
-            </div>
-            <div className="calcas-destaque">
-              <img src="" alt="blusa"/>
-              <h4>Camisetas</h4>
-            </div>
-            <div className="bone-destaque">
-              <img src="" alt="blusa"/>
-              <h4>Camisetas</h4>
-            </div>
-            <div className="headphone-destaque">
-              <img src="" alt="blusa"/>
-              <h4>Camisetas</h4>
-            </div>
-            <div className="tenis-destaque">
-              <img src="" alt="blusa"/>
-              <h4>Camisetas</h4>
-            </div>
-          </div>
-        </div>
-      </section>
+      <IconDestaque />
       <section className="container-produtos-em-alta">
         <div className="produtos-em-alta">
           <div className="topico-section">
